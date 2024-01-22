2a70c357403a6be40b3d130a8ef78d3eb8a4b413
王鹏杰 <18519266635@163.com>
2024-01-22 21:17:24 +0800

    update

--- OCR/test.py
+++ OCR/test.py
@@ -19,109 +19,18 @@ import pandas as pd
 """### Set arguments"""
 
 parser = argparse.ArgumentParser(description='Test on Chinese OCR')
-
-parser.add_argument('--lr', '--learning-rate', default=0.015, type=float, metavar='LR', help='initial learning rate',
-                    dest='lr')
-parser.add_argument('--epochs', default=601, type=int, metavar='N', help='number of total epochs to run')
-parser.add_argument('--batch-size', default=512, type=int, metavar='N', help='mini-batch size')
-parser.add_argument('--wd', default=5e-4, type=float, metavar='W', help='weight decay')
 # utils
-parser.add_argument('--resume', default='model3/model_last.pth', type=str, metavar='PATH', help='path to latest checkpoint (default: none)')
+parser.add_argument('--resume', default='model_last.pth', type=str, metavar='PATH', help='path to latest checkpoint (default: none)')
 parser.add_argument('--results-dir', default='test', type=str, metavar='PATH', help='path to cache (default: none)')
+parser.add_argument('--k', default=20, type=int)
 args = parser.parse_args()  # running in command line
+
 if args.results_dir == '':
     args.results_dir = './cache-' + datetime.now().strftime("%Y-%m-%d-%H-%M-%S-moco")
 print(args)
 args = parser.parse_args()  # running in command line
 
-
-class RandomGaussianBlur(object):
-    def __init__(self, p=0.5, min_kernel_size=3, max_kernel_size=21, min_sigma=0.1, max_sigma=1.6):
-        self.p = p
-        self.min_kernel_size = min_kernel_size
-        self.max_kernel_size = max_kernel_size
-        self.min_sigma = min_sigma
-        self.max_sigma = max_sigma
-
-    def __call__(self, img):
-        if random.random() < self.p and self.min_kernel_size < self.max_kernel_size:
-            kernel_size = random.randrange(self.min_kernel_size, self.max_kernel_size + 1, 2)
-            sigma = random.uniform(self.min_sigma, self.max_sigma)
-            return transforms.functional.gaussian_blur(img, kernel_size, sigma)
-        else:
-            return img
-
-def jioayan(image):
-    if np.random.random() < 0.5:
-        image1 = np.array(image)
-        # 添加椒盐噪声
-        salt_vs_pepper_ratio = np.random.uniform(0, 0.4)
-        amount = np.random.uniform(0, 0.01)
-        num_salt = np.ceil(amount * image1.size / 3 * salt_vs_pepper_ratio)
-        num_pepper = np.ceil(amount * image1.size / 3 * (1.0 - salt_vs_pepper_ratio))
-
-        # 在随机位置生成椒盐噪声
-        coords_salt = [np.random.randint(0, i - 1, int(num_salt)) for i in image1.shape]
-        coords_pepper = [np.random.randint(0, i - 1, int(num_pepper)) for i in image1.shape]
-        image1[coords_salt[0], coords_salt[1], :] = 255
-        image1[coords_pepper[0], coords_pepper[1], :] = 0
-        image = Image.fromarray(image1)
-    return image
-class TrainData(Dataset):
-    def __init__(self, transform=None):
-        super(TrainData, self).__init__()
-        with open('训练集.json', 'r') as f:
-            images = json.load(f)
-            labels = images
-        self.images, self.labels = images, labels
-        self.transform = transform
-
-    def __getitem__(self, item):
-        # 读取图片
-        image = Image.open(self.images[item]['path'])
-        # 转换
-        if image.mode == 'L':
-            image = image.convert('RGB')
-        x, y = 64,64
-        sizey, sizex = 129, 129
-        if y < 128:
-            while sizey > 128 or sizey <= 0:
-                sizey = round(random.gauss(y, 30))
-        if x < 128:
-            while sizex > 128 or sizex <= 0:
-                sizex = round(random.gauss(x, 30))
-        dx = 128 - sizex  # 差值
-        dy = 128 - sizey
-        xl = 0
-        yl = 0
-        if dx > 1:
-            while xl >= dx or xl <= 0:
-                xl = round(dx / 2)
-                xl = round(random.gauss(xl, 10))
-        if dy > 1:
-            while yl >= dy or yl <= 0:
-                yl = round(dy / 2)
-                yl = round(random.gauss(yl, 10))
-        yr = dy - yl
-        xr = dx - xl
-        image = jioayan(image)
-        random_gaussian_blur = RandomGaussianBlur()
-        image = random_gaussian_blur(image)
-        train_transform = transforms.Compose([
-            transforms.Resize((sizey, sizex)),
-            transforms.Pad([xl, yl, xr, yr], fill=(255, 255, 255), padding_mode='constant'),
-            transforms.RandomRotation(degrees=(-15, 15), center=(round(64), round(64)), fill=(255, 255, 255)),
-            transforms.RandomApply([transforms.ColorJitter(0.4, 0.4, 0.4, 0.1)], p=0.8),
-            transforms.RandomGrayscale(p=0.2),
-            transforms.ToTensor(),
-            transforms.Normalize([0.7814565, 0.7814565, 0.7814565], [0.3689568, 0.3689568, 0.3689568])])
-        image = train_transform(image)
-        label = torch.from_numpy(np.array(self.images[item]['label']))
-        return image, label
-
-    def __len__(self):
-        return len(self.images)
-with open('label对应表.json', 'r', encoding='utf8') as f:
+with open('label.json', 'r', encoding='utf8') as f:
     data = json.load(f)
 class TestData(Dataset):
     def __init__(self, transform=None):
@@ -134,24 +43,31 @@ class TestData(Dataset):
 
     def __getitem__(self, item):
         # 读取图片
-        # image = Image.open(self.images[item]['path'])
         image = Image.open(self.images[item])
         # 转换
         if image.mode == 'L':
             image = image.convert('RGB')
         # 获取当前图像的尺寸
         width, height = image.size
+        if width > height:
+            dy = width - height
 
-        # 缩小图像到一半尺寸
-        image = image.resize((width // 2, height // 2))
-        dx = 128 - width // 2  # 差值
-        dy = 128 - height // 2
-        xl = round(dx / 2)
-        yl = round(dy / 2)
-        yr = dy - yl
-        xr = dx - xl
+            yl = round(dy / 2)
+            yr = dy - yl
+            train_transform = transforms.Compose([
+                transforms.Pad([0, yl, 0, yr], fill=(255, 255, 255), padding_mode='constant'),
+            ])
+        else:
+            dx = height - width
+            xl = round(dx / 2)
+            xr = dx - xl
+            train_transform = transforms.Compose([
+                transforms.Pad([xl, 0, xr, 0], fill=(255, 255, 255), padding_mode='constant'),
+            ])
+
+        image = train_transform(image)
         train_transform = transforms.Compose([
-            transforms.Pad([xl, yl, xr, yr], fill=(255, 255, 255), padding_mode='constant'),
+            transforms.Resize((128, 128)),
             transforms.ToTensor(),
             transforms.Normalize([0.7760929, 0.7760929, 0.7760929], [0.39767382, 0.39767382, 0.39767382])])
         image = train_transform(image)
@@ -161,7 +77,7 @@ class TestData(Dataset):
         return len(self.images)
 
 test_dataset = TestData()
-test_loader = DataLoader(test_dataset, shuffle=False, batch_size=128, num_workers=16, pin_memory=True)
+test_loader = DataLoader(test_dataset, shuffle=False, batch_size=128, num_workers=0, pin_memory=True)
 
 
 class Residual(nn.Module):
@@ -225,10 +141,6 @@ def init_weights(m):
         nn.init.xavier_uniform_(m.weight)
 
 
-optimizer = torch.optim.SGD(net.parameters(), lr=args.lr, weight_decay=0.0005, momentum=0.9)
-loss = nn.CrossEntropyLoss()
-
-
 def adjust_learning_rate(optimizer, epoch, args):
     """Decay the learning rate based on schedule"""
     lr = args.lr
@@ -249,31 +161,6 @@ def accuracy(y_hat, y):
     return float(torch.sum(cmp).item())
 
 
-def train(net, data_loader, train_optimizer, epoch, args):
-    net.train()
-    adjust_learning_rate(optimizer, epoch, args)
-    total_loss, total_num, trainacc, train_bar = 0.0, 0, 0.0, tqdm(data_loader)
-    for image, label in train_bar:
-        image, label = image.cuda(0), label.cuda(0)
-
-        y_hat = net(image)
-
-        train_optimizer.zero_grad()
-        l = loss(y_hat, label)
-        l.backward()
-        train_optimizer.step()
-        trainacc += accuracy(y_hat, label)
-        total_num += data_loader.batch_size
-        total_loss += l.item() * data_loader.batch_size
-        train_bar.set_description(
-            'Train Epoch: [{}/{}], lr: {:.6f}, Loss: {:.4f}, trainacc: {:.6f}'.format(epoch, args.epochs,
-                                                                                      optimizer.param_groups[0]['lr'],
-                                                                                      total_loss / total_num,
-                                                                                      trainacc / total_num))
-
-    return total_loss / total_num, trainacc / total_num
-
-
 def test(net, test_data_loader):
     net.eval()
     testacc, total_top5, total_num, test_bar = 0.0, 0.0, 0, tqdm(test_data_loader)
@@ -283,19 +170,23 @@ def test(net, test_data_loader):
         for image,path in test_bar:
             image = image.cuda(0)
             y_hat = net(image)
-            y_hat = torch.argmax(y_hat, dim=1)
-            label = [int(x) for x in y_hat.tolist()]
+            # y_hat = torch.argmax(y_hat, dim=1)
+            y_hat = torch.topk(y_hat, args.k, dim=1)[1]
+            label = y_hat.tolist()
             labellist=labellist+label
             path=list(path)
             pathlist=pathlist+path
 
         dataset={}
         for i in range(len(pathlist)):
+            path_label=[]
             path=pathlist[i]
-            label=str(labellist[i])
-            label=data[label]
-            dataset[path]=label
-        with open('result.json', 'w') as f:
+            label=labellist[i]
+            for j in label:
+                j=str(j)
+                path_label.append(data[j])
+            dataset[path]=path_label
+        with open('result.json', 'w',encoding='utf8') as f:
             json.dump(dataset, f, ensure_ascii=False)
     return
 
@@ -305,7 +196,7 @@ epoch_start = 1
 if args.resume != '':
     checkpoint = torch.load(args.resume)
     net.load_state_dict(checkpoint['state_dict'])
-    optimizer.load_state_dict(checkpoint['optimizer'])
+    # optimizer.load_state_dict(checkpoint['optimizer'])
     epoch_start = checkpoint['epoch'] + 1
     print('Loaded from: {}'.format(args.resume))
 else:
